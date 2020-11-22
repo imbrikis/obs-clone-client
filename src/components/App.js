f2a615ab4702edf73eef94e615275c012c377f15
imbrikis <imbrikis@yahoo.com>
2020-11-22 14:42:19 -0700

    lesson 286

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const PageOne = () => {
+  return (
+    <div>PageOne</div>
+  )
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      <div>PageTwo</div>
+      <button>Click Me</button>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
