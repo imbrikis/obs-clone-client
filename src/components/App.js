bb18b226bf85f6ff390a578b7a082256b2c03c4e
imbrikis <imbrikis@yahoo.com>
2020-11-22 19:31:29 -0700

    lesson 296

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
@@ -11,9 +12,9 @@ import Header from './Header'
 const App = () => {
   return (
     <div>
-      <Header className="ui container" />
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList}></Route>
           <Route path="/streams/new" exact component={StreamCreate}></Route>
           <Route path="/streams/edit" exact component={StreamEdit}></Route>
